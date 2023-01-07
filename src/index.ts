148606cab1724cc5f1a4360d2833de55895863c5
Vanshul <vanshul.ahuja@gmail.com>
2023-01-07 18:26:03 +0530

    Changed port to process env

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.json());
 
+const port = process.env.PORT || 8080;
+
 app.post('/users/addBatch', controller.addUsers);
 app.use('/health', (req, res) => {
   return res.status(200).json('ok');
@@ -14,6 +16,6 @@ app.use('/', (req, res) => {
   return res.status(404).json('NOT FOUND');
 });
 
-app.listen(8080, () => {
-  console.log('Server Running on Port: 8080');
+app.listen(port, () => {
+  console.log(`Server Running on Port: ${port}`);
 });
