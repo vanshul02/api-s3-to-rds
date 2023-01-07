28b4ea0c0f600a9a112fa40dc3c042ad35b1f5a9
Vanshul <vanshul.ahuja@gmail.com>
2023-01-07 18:23:48 +0530

    Added health check path

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.json());
 
 app.post('/users/addBatch', controller.addUsers);
+app.use('/health', (req, res) => {
+  return res.status(200).json('ok');
+});
+
+app.use('/', (req, res) => {
+  return res.status(404).json('NOT FOUND');
+});
 
 app.listen(8080, () => {
   console.log('Server Running on Port: 8080');
